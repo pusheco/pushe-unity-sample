04cb0e8f09fb31f155ce0e42ea61564b2e1d6951
mhahmadip <mohammad.ahmadi@ronash.co>
2015-11-28 15:39:14 +0330

    Unity sample project added

---
+++ pusheTest/Assets/Source/Pushe.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pushe : MonoBehaviour 
+{
+	private AndroidJavaObject activityContext = null;
+	public bool showGooglePlayDialog = true;
+	public string channel = "pusheTestChannel";
+
+
+	void Start() 
+    {
+		try
+		{
+			AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			
+			//getting context of unity activity
+			activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+			//calling plugin class by package name
+			AndroidJavaClass pluginClass = new AndroidJavaClass("co.ronash.pushe.Pushe");
+			
+			if (pluginClass != null)
+			{
+				activityContext.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+				{
+                    //calling initialize static method
+                    pluginClass.CallStatic("initialize", new object[2] { activityContext, showGooglePlayDialog });
+					// subscribe to a channel
+					pluginClass.CallStatic("subscribe", new object[2] { activityContext, channel});
+				}));
+			}
+		}
+		catch
+		{
+		}
+	}
+
+}
