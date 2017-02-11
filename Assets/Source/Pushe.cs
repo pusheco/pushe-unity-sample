b6db6a97ae6765651a1b31e832b44f8be2714b79
Akram Shokri <Merka@Akrams-MBP.lan>
2017-02-11 17:23:18 +0330

    PU-1981 updating unity sample on github

--- Assets/Source/Pushe.cs
+++ Assets/Source/Pushe.cs
@@ -68,14 +68,12 @@ public class Pushe : MonoBehaviour
 					{
 						//calling initialize static method
 						pluginClass.CallStatic("initialize", new object[2] { activityContext, showGooglePlayDialog });
-						// subscribe to a channel
-						//pluginClass.CallStatic("subscribe", new object[2] { activityContext, channel});
-						// unsubscribe from a channel
-						//pluginClass.CallStatic("unsubscribe", new object[2] { activityContext, "subscribed channel name"});
 					} ) );
 
 			}
-			Subscribe("unity_test_topic");
+			//sample usage of Subscribe method: use it after a chack on 'PusheIsInitialized()'
+			//if(PusheIsInitialized())
+			//	Subscribe("unity_test_topic");
 		}
 		catch
 		{
@@ -115,7 +113,7 @@ public class Pushe : MonoBehaviour
 	/**
 	 * Call this method to enable publishing notification to user, if you already called SetNotificationOff()
 	 **/
-	public static void SetNotificationOn(){
+	public static void NotificationOn(){
 		AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
 		AndroidJavaObject context = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
 		AndroidJavaClass pluginClass = new AndroidJavaClass("co.ronash.pushe.Pushe");
@@ -128,13 +126,27 @@ public class Pushe : MonoBehaviour
 	 * Call this method to disable publishing notification to user.
 	 * To enable showing notifications again, you need to call SetNotificationOn()
 	 **/
-	public static void SetNotificationOff(){
+	public static void NotificationOff(){
 		AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
 		AndroidJavaObject context = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
 		AndroidJavaClass pluginClass = new AndroidJavaClass("co.ronash.pushe.Pushe");
 		if (pluginClass != null) {
 			pluginClass.CallStatic("setNotificationOff", new object[1] {context});
 		}
+	}
+
+	/**
+	 * Call this method to disable publishing notification to user.
+	 * To enable showing notifications again, you need to call SetNotificationOn()
+	 **/
+	public static bool PusheIsInitialized(){
+		AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject context = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+		AndroidJavaClass pluginClass = new AndroidJavaClass("co.ronash.pushe.Pushe");
+		if (pluginClass != null) {
+			return pluginClass.CallStatic<bool>("isPusheInitialized", new object[1] {context});
+		}
+		return false;
 	}
 
 }
